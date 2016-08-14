b088eb6067204f2fb322dfbb47b87a8c389f28da
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-14 01:35:47 -0700

    Added own sidebar and toolbar components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,28 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import Navbar from '../components/navbar/Navbar'
+import Navbar from '../components/navbar/Navbar';
+import { Sidebar } from 'react-responsive-sidebar';
+import { Toolbar, ToolbarGroup, ToolbarItem } from 'react-responsive-toolbar';
+
+const toolbarItems = [
+  <ToolbarItem>Greg Chamberlain</ToolbarItem>,
+  <ToolbarGroup float="right">
+    <ToolbarItem>About</ToolbarItem>
+    <ToolbarItem>Project</ToolbarItem>
+  </ToolbarGroup>
+];
 
 class App extends Component {
 
   render() {
     const { children } = this.props;
     return (
-      <Navbar>
-        {children}
-      </Navbar>
+      <Sidebar items={[]}>
+        <Toolbar sidebarIcon={true} items={toolbarItems}>
+          {children}
+        </Toolbar>
+      </Sidebar>
     );
   }
 }
